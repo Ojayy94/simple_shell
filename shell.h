950eb3510faf51a719b1a9bef66a36e8ec6a1d7e
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-11-09 02:10:51 -0800

    header

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+extern char **environ;
+
+
+#endif /* SHELL_H */
