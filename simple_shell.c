bc2c614ba31d0c53129baf48c36daaa2918aa594
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-11-15 07:08:56 -0800

    main shell

--- simple_shell.c
+++ simple_shell.c
@@ -27,7 +27,7 @@ int run_shell(void)
 		if (n > 0)
 			input[n - 1] = '\0';
 		if (strcmp(input, "exit") == 0)
-			free(input), return (0);
+			free(input), exit_shell();
 		else if (strcmp(input, "env") == 0)
 			env_shell();
 		else
